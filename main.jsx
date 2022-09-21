8c00198c7bbc5bbb337acc937a1450fc82dff07b
yanjixiong <yanjixiong@shimo.im>
2022-09-21 19:06:21 +0800

    feat: use npm react ssr demo

---
+++ main.jsx
@@ -0,0 +1,22 @@
+// ssr.jsx
+import * as React from "npm:react";
+import { renderToReadableStream } from "npm:react-dom/server";
+
+const App = () => (
+  <html>
+    <body>
+      <h1>Hello World</h1>
+    </body>
+  </html>
+);
+
+const options = {
+  headers: {
+    "Content-Type": "text/html",
+  },
+};
+
+Deno.serve(
+  { port: 80 },
+  async () => new Response(await renderToReadableStream(<App />), options),
+);
